2cb28e6f79c763f9f64c5a6e5388934f0e655d7c
kiqueangulo <kiqueangulo@gmail.com>
2022-03-23 20:06:25 -0400

    Comleted

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+
+    playGame(betAmaount) {
+        this.timesPlayed++;
+
+        if (Math.random() <= 0.5 || this.isFakeCoin === true) {
+            console.log(`${this.name} wins!`);
+        } else {
+            console.log(`You won ${betAmaount * (this.timesPlayed)}`);
+        };
+    };
+
+    rollDie(sides) {
+        console.log('You rolled a ' + Math.ceil(Math.random() * 6) + '!');
+    };
 };
 
 // TESTS
@@ -11,20 +29,18 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
